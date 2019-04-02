78679d4d5a6a5635c299a4f32f81e84aa602668a
Shairyar Baig <shairyarbaig@gmail.com>
2019-04-02 15:39:43 +0500

    pages scafolded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,13 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'login', loadChildren: './pages/authentication/login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './pages/authentication/register/register.module#RegisterPageModule' },
+  { path: 'index', loadChildren: './pages/articles/index/index.module#IndexPageModule' },
+  { path: 'show', loadChildren: './pages/articles/show/show.module#ShowPageModule' },
+  { path: 'create', loadChildren: './pages/articles/create/create.module#CreatePageModule' },
+  { path: 'update', loadChildren: './pages/articles/update/update.module#UpdatePageModule' }
 ];
 @NgModule({
   imports: [
